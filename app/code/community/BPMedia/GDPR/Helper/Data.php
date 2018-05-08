2a5f9fd42a0c80143a2ea9a109e8c86b0cc8f217
wrglbr dev <offerhero@gmail.com>
2018-05-08 01:39:33 -0500

    update admin

--- app/code/community/BPMedia/GDPR/Helper/Data.php
+++ app/code/community/BPMedia/GDPR/Helper/Data.php
@@ -1,11 +1,17 @@
 <?php
 class BPMedia_GDPR_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    const XML_PATH_GDPR_ENABLED = 'bpmedia_gdpr/configuration/enabled';
+    const XML_PATH_GDPR_ENABLED = 'bpmedia_gdpr/configuration/cookienotice';
+    const XML_PATH_GDPR_ANONENABLED = 'bpmedia_gdpr/configuration/enabled';
     const XML_PATH_INFORMATION_PAGE = 'bpmedia_gdpr/configuration/cms_page';
     const XML_PATH_GDPR_SUCCESSMESSAGE = 'bpmedia_gdpr/configuration/successmessage';
 
     public function isEnabled()
+    {
+        return (bool)Mage::getStoreConfig(self::XML_PATH_GDPR_COOKIENOTICE);
+    }
+
+    public function isAnonEnabled()
     {
         return (bool)Mage::getStoreConfig(self::XML_PATH_GDPR_ENABLED);
     }
